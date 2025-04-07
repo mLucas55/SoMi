68ed99f81760dfe9f6a85ce4ec4ac126334999ad
mLucas55 <lucasmonroe66@gmail.com>
2025-04-07 11:56:54 -0400

    Documented remove_image.py

--- remove_image.py
+++ remove_image.py
@@ -1,5 +1,7 @@
 import os
 
+# Helper function to remove an image files after processing
+
 def remove_image(input_path, output_path):
     if os.path.exists(output_path):
         os.remove(input_path)
